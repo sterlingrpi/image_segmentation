79aa29d61c4df2409bbf5e5cd13383dc89cead29
Sterling <sterling.nesbitt@gmail.com>
2020-08-30 10:50:49 -0400

    updating arguments for import images method

--- tflite_test.py
+++ tflite_test.py
@@ -3,17 +3,24 @@ import numpy as np
 from matplotlib import pyplot
 from import_data import import_images
 
-image_size = (256, 256)
-data_dir = 'D:/PycharmProjects/ssd-tf2/dataset/bird_images'
-images, labels, num_classes, weights = import_images(image_size, data_dir, augment=False)
+image_dir = 'D:/PycharmProjects/image_segmentation/bird_images'
+label_dir = 'D:/PycharmProjects/image_segmentation/bird_labels'
+image_size = (512, 512)
+label_size = (128, 128)
+images, labels, num_classes, weights = import_images(image_size, label_size, image_dir, label_dir, augment=True)
 
 np.random.shuffle(images)
 
-tflite_model_path='./seg.tflite'
+tflite_model_path='./models/seg_512_128.tflite'
 interpreter = tf.lite.Interpreter(model_path = tflite_model_path)
 interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
+
+image_size = input_details[0]['shape']
+print('image_size =', image_size)
+print('output_details[0][shape] = ', output_details[0]['shape'][3])
+
 for image in images:
     data = [image.astype('float32')]
     interpreter.set_tensor(input_details[0]['index'], data)
